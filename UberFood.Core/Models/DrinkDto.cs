328c45ccc87e3d284f74be1b83c23b5c51ba0257
Axel <contact@axeljoly.fr>
2025-09-03 15:15:23 +0200

    optimisation class Dto to public

--- UberFood.Core/Models/DrinkDto.cs
+++ UberFood.Core/Models/DrinkDto.cs
@@ -4,9 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UberFood.Core.Models
+namespace UberFood.Core.Models;
+
+public class DrinkDto
 {
-    internal class DrinkDto
-    {
-    }
 }
