43f478bfa243f6b4692aa830cf5b3b81c4651281
Guillaume <guillaume.mene@gmail.com>
2025-09-04 15:56:01 +0200

    creation de adresshandler et userhandler

---
+++ UberFood.Core/Handlers/AdressHandler.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UberFood.Core.Context;
+using UberFood.Core.Models;
+
+namespace UberFood.Core.Handlers;
+
+public class AdressHandler
+{
+    public void AddAdress(AdressDto adress)
+    {
+        try
+        {
+            using (var ctx = new DataContext())
+            {
+                var adressToAdd = new Entities.Adress
+                {
+                    Id = adress.Id,
+                    Street = adress.Street,
+                    City = adress.City,
+                    State = adress.State,
+                    Zip = adress.Zip,
+                    Country = adress.Country,
+
+                };
+
+                ctx.Add(adressToAdd);
+                ctx.SaveChanges();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+    public List<AdressDto> GetAdresses()
+    {
+        try
+        {
+            using (var ctx = new DataContext())
+            {
+                var adress = ctx.Adresses
+                    .Select(p => new AdressDto(p.Street, p.City, p.State, p.Zip, p.Country, p.Id))
+                    .ToList();
+
+                return adress;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return [];
+        }
+    }
+    public bool DeleteAdresses(int id)
+    {
+        try
+        {
+            using (var ctx = new DataContext())
+            {
+                var adressToRemove = ctx.Adresses.FirstOrDefault(p => p.Id == id);
+                if (adressToRemove is not null)
+                {
+                    ctx.Remove(adressToRemove);
+                    ctx.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return false;
+        }
+    }
+}
