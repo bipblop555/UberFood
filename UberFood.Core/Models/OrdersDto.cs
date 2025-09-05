24545fd47813af7c066bdfd4719852eaaeb0b441
Guillaume <guillaume.mene@gmail.com>
2025-09-05 12:17:00 +0200

    creation orderhandler

--- UberFood.Core/Models/OrdersDto.cs
+++ UberFood.Core/Models/OrdersDto.cs
@@ -8,6 +8,8 @@ namespace UberFood.Core.Models;
 
 public class OrdersDto
 {
+    private DateTime deliverydate;
+
     public OrdersDto(DateTime orderdate, DateTime deliverydate,int status,int id) 
     {
         this.OrderDate = orderdate;
@@ -15,6 +17,28 @@ public class OrdersDto
         this.Status = status;
         this.Id = id;
     }
+
+    public OrdersDto(int userId, int AdressId, DateTime orderdate, DateTime deliverydate, int status)
+    {
+        UserId = userId;
+        this.AdressId = AdressId;
+        OrderDate = orderdate;
+        this.deliverydate = deliverydate;
+        Status = status;
+    }
+
+    public OrdersDto(int userId, int AdressId, DateTime orderdate, DateTime deliverydate, int status, int id)
+    {
+        this.UserId = userId;
+        this.AdressId = AdressId;
+        this.OrderDate = orderdate;
+        this.DelivryDate = deliverydate;
+        this.Status = status;
+        this.Id = id;
+    }
+    public int UserId { get; set; }
+    public int AdressId { get; set; }
+
     public DateTime OrderDate { get; set; }
     public DateTime DelivryDate { get; set; }
     public int Status { get; set; }
