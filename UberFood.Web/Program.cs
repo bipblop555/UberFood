0ccff8495e11e2c1b56d17c0f88ed542901631d3
bipblop555 <tom.bonnet@hotmail.com>
2025-10-01 16:39:39 +0200

    ajout service prgm.cs

--- UberFood.Web/Program.cs
+++ UberFood.Web/Program.cs
@@ -24,7 +24,11 @@ builder.Services.AddHttpClient<IPastaService, PastaService>(client =>
     client.BaseAddress = new Uri("https://localhost:7018/api/pasta");
 
 });
+builder.Services.AddHttpClient<IBurgersService, BurgersService>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7018/api/burgers");
 
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
