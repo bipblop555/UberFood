a902412cf600953c800c6f717ea583225c9807b3
Axel <contact@axeljoly.fr>
2025-10-03 13:24:27 +0200

    finalisationDetailsOrder

--- UberFood.Web/ViewsModel/OrderProducts/OrderProductViewModel.cs
+++ UberFood.Web/ViewsModel/OrderProducts/OrderProductViewModel.cs
@@ -1,8 +1,12 @@
-﻿namespace UberFood.Web.ViewsModel.OrderProducts
+﻿using System.ComponentModel;
+
+namespace UberFood.Web.ViewsModel.OrderProducts
 {
     public class OrderProductViewModel
     {
+        [DisplayName("Nom du Produit")]
         public string ProductName { get; set; } = string.Empty;
+        [DisplayName("Prix du Produit")]
         public double ProductPrice { get; set; }
 
     }
